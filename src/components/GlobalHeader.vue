f6690edb323c689cb6ee7ed5731f3f6511f9e3d4
yvchuan <27225582339@qq.com>
2025-03-19 16:56:48 +0800

    test

---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="basicLayout">
+    <a-layout style="height: 100vh">
+      <a-layout-header class="header">Header</a-layout-header>
+      <a-layout-content class="content">
+        <router-view />
+      </a-layout-content>
+      <a-layout-footer class="footer">
+        <a>author: 宇川(Chozen James)</a>
+      </a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+#basicLayout {
+}
+
+#basicLayout .content {
+  background: linear-gradient(to right, #fefefe, #fff);
+  margin-bottom: 28px;
+  padding: 20px;
+}
+
+.footer {
+  background: #efefef;
+  padding: 16px;
+  text-align: center;
+}
+</style>
